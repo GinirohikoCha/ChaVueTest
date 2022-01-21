34322dcd23e29123f28c92d9111269e7ebed74fb
GinirohikoCha <rinkouatf@qq.com>
2022-01-21 16:19:21 +0800

    update & test cha-publish-test

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 
 import ImportExcel from 'cha-publish-test'
+import 'cha-publish-test/dist/cha-publish-test.css'
 
 createApp(App).use(ImportExcel).use(store).use(router).mount('#app')
